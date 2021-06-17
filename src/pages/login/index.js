254d32c3923e8d26251c04e8566a235ea1faae9a
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-17 00:11:07 -0300

    feat: add Login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,72 @@
-import {UnauthenticatedLayout} from '../../components'
+import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
+import { loginValidations } from '../../helpers'
+import { UnauthenticatedLayout, TextInput, Button } from '../../components'
 import './styles.css'
 
 export const Login = (props) => {
+
+
+    const handleLogin = (payload) => {
+        console.log(payload)
+    }
+
+    const { values, handleSubmit, handleChange, errors } = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: loginValidations(),
+        validateOnBlur: false,
+        validateOnChange: false,
+        onSubmit: () => handleLogin({
+            name: values.name,
+            phone: values.phone,
+            email: values.email
+        })
+    })
+
     return (
         <UnauthenticatedLayout>
-            login
+            <div className="login-controller">
+                <div>
+                    <header>
+                        <h2>Login</h2>
+                    </header>
+
+                    <div className="input-control">
+                        <TextInput
+                            id="email"
+                            name="email"
+                            type="email"
+                            placeholder="e-mail"
+                            onChange={handleChange}
+                            error={errors.email}
+                        />
+                    </div>
+                    <div className="input-control">
+                        <TextInput
+                            id="password"
+                            name="password"
+                            type="password"
+                            placeholder="password"
+                            onChange={handleChange}
+                            error={errors.password}
+                        />
+                        <div className="text-to-right">
+                            <p>forgot password? <Link className="highlight" to="/forgot-password">click here</Link></p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <Button 
+                        kind="primary"
+                        onClick={handleSubmit}
+                    >
+                        sign in
+                    </Button>
+                </div>
+            </div>
         </UnauthenticatedLayout>
     )
 }
