12a4df326cf1292f1d029f6b413303ac1c27c68d
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-23 15:41:58 -0300

    feat: add switcher for user role

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,35 @@
-// import * as Styles from './styles'
+import { ROLES } from 'helpers/home'
+import { useUser } from 'hooks/user-hook'
+import { toast } from 'react-toastify'
+import { RegularHome, AuthenticatedLayout } from 'components'
+import { useHistory } from 'react-router-dom'
 
-export const Home = (props) => {
-    return (
-        <div>
-            Home
-        </div>
-    )
+export const Home = () => {
+  const { user } = useUser()
+  const history = useHistory()
+  console.log(user)
+
+  const userRoleController = () => {
+    switch (user.role) {
+      case ROLES.regular:
+        return <RegularHome />
+      case ROLES.owner:
+        // return <OwnerHome />
+        break
+      case ROLES.admin:
+        // return <AdminHome />
+        break
+      default:
+        history.push('/')
+        toast.error('Oopps something wrong with your role! Please contact and admin')
+    }
+  }
+
+  return (
+    <AuthenticatedLayout userName={user.name}>
+      {userRoleController()}
+    </AuthenticatedLayout>
+  )
 }
 
 Home.path = '/home'
