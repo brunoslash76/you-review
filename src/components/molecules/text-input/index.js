a10f3524136b5bc755092dcc2d5057f30f8a56f9
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-22 22:52:47 -0300

    feat: add toggle button to show/hide password

--- src/components/molecules/text-input/index.js
+++ src/components/molecules/text-input/index.js
@@ -1,13 +1,33 @@
+import { useState } from 'react'
 import './styles.css'
+import openEyeIcon from 'public/visible.png'
+import closedEyeIcon from 'public/not-visible.png'
 
 export const TextInput = (props) => {
-    const { error } = props
-    return (
-        <div className="text-input--container">
-            <input
-                {...props}
+  const { error, type } = props
+  const [isPasswordVisible, setPasswordVisible] = useState(true)
+  const shouldShowPasswordButton = type === 'password'
+  return (
+    <div className="text-input--container">
+      <input
+        {...props}
+        type={shouldShowPasswordButton && isPasswordVisible ? type : 'text'}
+      />
+      {shouldShowPasswordButton
+        && (
+          <button
+            className="text-input-buttom"
+            type="button"
+            onClick={() => setPasswordVisible(state => !state)}
+          >
+            <img
+              src={isPasswordVisible ?  openEyeIcon : closedEyeIcon}
+              alt="Show password icon, an open or closed eye"
             />
-            <div className="text-input--error" data-testId="error-message" >{error && error}</div>
-        </div>
-    )
+          </button>
+        )
+      }
+      {error && <div className="text-input--error" >{error}</div>}
+    </div>
+  )
 }
