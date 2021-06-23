eb69cd19797c11aa1f6ae62594a12c66954d0c06
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-23 15:48:27 -0300

    feat: add restaurant image placeholder

---
+++ src/components/atoms/restaurant-placeholder/index.js
@@ -0,0 +1,8 @@
+import shop from 'public/shop.png'
+import './styles.css'
+
+export const RestaurantPlaceholder = () => (
+  <div className="placeholder-container">
+    <img src={shop} alt="A draw of a restaurante outlined" />
+  </div>
+)
