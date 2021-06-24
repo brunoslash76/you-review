c7dfb6f95d75b5c5c44ffe99f9f4a0398ef44680
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-24 16:54:37 -0300

    feat: add navigation function for AuthenticatedLayout

--- src/components/layout/authenticated-layout/index.js
+++ src/components/layout/authenticated-layout/index.js
@@ -1,16 +1,18 @@
-import { useUser } from 'hooks/user-hook'
 import { RatingStars } from 'components/molecules'
 import './styles.css'
 
 export const AuthenticatedLayout = (props) => {
-  const { children, title, restaurant } = props
-  const { user } = useUser()
+  const { children, title, restaurant, navigation } = props
+
   return (
     <div className="auth-wrapper">
       <div className="auth-container">
         <section className="auth-content">
           <header className="auth-header">
-            <h1 className="auth-h1">{title ? title : 'You Review'}</h1>
+            <div className="auth-header-container">
+              {navigation && navigation()}
+              <h1 className="auth-h1">{title ? title : 'You Review'}</h1>
+            </div>
             <div className={!!restaurant ? "auth-subtitle" : "auth-name-right"}>
               {
                 restaurant
@@ -21,7 +23,6 @@ export const AuthenticatedLayout = (props) => {
                   </div>
                 )
               }
-              <p>Hello {user.name}!</p>
             </div>
           </header>
           <main>
