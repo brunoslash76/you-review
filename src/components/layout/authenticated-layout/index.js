2fdb088f072b8554c713f8f2f288709fbec5fa23
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-23 23:40:23 -0300

    feat: add subtitle section

--- src/components/layout/authenticated-layout/index.js
+++ src/components/layout/authenticated-layout/index.js
@@ -1,15 +1,28 @@
+import { useUser } from 'hooks/user-hook'
+import { RatingStars } from 'components/molecules'
 import './styles.css'
 
 export const AuthenticatedLayout = (props) => {
-  const { children, userName } = props
-
+  const { children, title, restaurant } = props
+  const { user } = useUser()
   return (
     <div className="auth-wrapper">
       <div className="auth-container">
         <section className="auth-content">
           <header className="auth-header">
-            <h1 className="auth-h1">You Review</h1>
-            <p>Hello {userName}!</p>
+            <h1 className="auth-h1">{title ? title : 'You Review'}</h1>
+            <div className={!!restaurant ? "auth-subtitle" : "auth-name-right"}>
+              {
+                restaurant
+                && (
+                  <div className="auth-restaurant-avaliation">
+                    <RatingStars averageRating={restaurant.average_rating} />
+                    <p>of <strong>{restaurant.total_reviews}</strong> avaliations</p>
+                  </div>
+                )
+              }
+              <p>Hello {user.name}!</p>
+            </div>
           </header>
           <main>
             {children}
