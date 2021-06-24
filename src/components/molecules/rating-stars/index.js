4b9b2b3d508af0b3a3e900d1cc5e22aa601e378a
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-24 17:08:30 -0300

    feat: add usecase for zero ratings

--- src/components/molecules/rating-stars/index.js
+++ src/components/molecules/rating-stars/index.js
@@ -1,16 +1,22 @@
 import star from 'public/star.png'
+import starOutlined from 'public/star-outline.png'
 import './styles.css'
 
 export const RatingStars = (props) => {
 
   const { averageRating } = props
-  console.log(averageRating)
 
   const renderStars = () => {
     const stars = []
 
     for (let i = 0; i < averageRating; i++) {
       stars.push(<img className="image-star" key={`star_${i}`} src={star} alt="A star" />)
+    }
+
+    if (averageRating === 0) {
+      for (let i = 0; i < 5; i++) {
+        stars.push(<img className="image-star" key={`star_${i}`} src={starOutlined} alt="A star" />)
+      }
     }
 
     return stars
