ab79d1c676b39b80e5ed066ae42566df2bcfe889
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-26 20:10:13 -0300

    feat: add OwnerHome and AdminHome to Home container

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,37 +1,31 @@
 import { ROLES } from 'helpers/home'
 import { useUser } from 'hooks/user-hook'
 import { toast } from 'react-toastify'
-import { RegularHome, AuthenticatedLayout } from 'components'
+import { AdminHome, RegularHome, OwnerHome } from 'components'
 import { useHistory } from 'react-router-dom'
 
 export const Home = () => {
   const { user } = useUser()
   const history = useHistory()
 
-  const userRoleController = () => {
-    if (!user) return history.push('/')
-    switch (user.role) {
-      case ROLES.regular:
-        return <RegularHome />
-      case ROLES.owner:
-        // return <OwnerHome />
-        break
-      case ROLES.admin:
-        // return <AdminHome />
-        break
-      default:
-        history.push('/')
-        toast.error('Oopps something wrong Please contact an admin')
-    }
+  if (!user) {
+    history.push('/')
+    return null
   }
-
-  return (
-    <AuthenticatedLayout>
-      {userRoleController()}
-    </AuthenticatedLayout>
-  )
+  switch (user.role) {
+    case ROLES.regular:
+      return <RegularHome />
+    case ROLES.owner:
+      return <OwnerHome />
+    case ROLES.admin:
+      return <AdminHome />
+    default:
+      history.push('/')
+      toast.error('Oopps something wrong Please contact an admin')
+      return null
+  }
 }
 
 Home.path = '/home'
-Home.secure = false
+Home.secure = true
 Home.title = 'Home'
