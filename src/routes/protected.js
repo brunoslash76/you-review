afb91ae2426b3b1710d0ba95147bb04438cf6e96
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-26 20:04:58 -0300

    feat: add lock to router if no user logged

--- src/routes/protected.js
+++ src/routes/protected.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import { useUser } from 'hooks/user-hook'
 
 
 export const Protected = ({ children, ...props }) => {
+  const { user } = useUser()
 
   return (
     <Route
       {...props}
       render={() => {
-        if (true) {
+        if (!user) {
           return <Redirect to={{ pathname: '/login' }} />
         }
 
