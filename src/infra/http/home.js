23e2454ae217db18d23685bcc023473ffddb4503
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-26 20:08:58 -0300

    feat: add methods get and delete to home http methods

--- src/infra/http/home.js
+++ src/infra/http/home.js
@@ -1,14 +1,23 @@
-const { get, put } = require('helpers/http-helpers')
+const { get, put, dellete } = require('helpers/http-helpers')
 
-const URL = '/restaurants'
+const RESTAURANT_URL = '/restaurants'
 const REVIEWS_URL = '/reviews'
+const USERS_URL = '/users'
 
 export const getRestaurants = () => {
-    return get(URL)
+    return get(RESTAURANT_URL)
+}
+
+export const getUsers = () => {
+    return get(USERS_URL)
+}
+
+export const getReviews = () => {
+    return get(REVIEWS_URL)
 }
 
 export const getOwnerRestaurants = (userId) => {
-    return get(`${URL}?owner_id=${userId}`)
+    return get(`${RESTAURANT_URL}?owner_id=${userId}`)
 }
 
 export const getPendingReview = (restaurantId) => {
@@ -17,4 +26,16 @@ export const getPendingReview = (restaurantId) => {
 
 export const updateReviews = (reviewId, payload) => {
     return put(`${REVIEWS_URL}/${reviewId}`, payload)
+}
+
+export const deleteReview = (reviewId) => {
+    return dellete(`${REVIEWS_URL}/${reviewId}`)
+}
+
+export const deleteUser = (userId) => {
+    return dellete(`${USERS_URL}/${userId}`)
+}
+
+export const deleteRestaurant = (restaurantId) => {
+    return dellete(`${RESTAURANT_URL}/${restaurantId}`)
 }
