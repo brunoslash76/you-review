ed9d3a057b88a95f39b7a9d173f449ca5ab4d113
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-26 20:06:55 -0300

    feat: add updateRestaurantReview http method

--- src/infra/http/restaurant-review.js
+++ src/infra/http/restaurant-review.js
@@ -1,7 +1,28 @@
-const { post } = require("helpers/http-helpers")
+const { post, get, put } = require("helpers/http-helpers")
 
 const REVIEWS_URL = '/reviews'
+const RESTAURANT_URL = '/restaurants'
 
 export const postReview = (body) => {
     return post(REVIEWS_URL, body)
+}
+
+export const updateRestaurantReviews = async (restaurant) => {
+    try {
+        const { data } = await get(`${REVIEWS_URL}?restaurant_id=${restaurant.id}`)
+        console.log(data)
+        let sum = 0
+        for (let i = 0; i < data.length; i++) {
+            sum += data[i].ratting
+        }
+        debugger
+        const average = Math.floor(sum / data.length)
+
+        restaurant.average_rating = average
+        restaurant.total_reviews = restaurant.total_reviews + 1
+        await put(`${RESTAURANT_URL}/${restaurant.id}`, restaurant)
+
+    } catch(error) {
+        throw new Error(error)
+    }
 }
