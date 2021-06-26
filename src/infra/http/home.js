e2d454e2b68fd20ec42d81064a89efc4bda02940
Bruno Russo <btuno.russo@adventhealth.com>
2021-06-26 11:21:16 -0300

    feat: add http methods to home

--- src/infra/http/home.js
+++ src/infra/http/home.js
@@ -1,7 +1,20 @@
-const { get } = require('helpers/http-helpers')
+const { get, put } = require('helpers/http-helpers')
 
 const URL = '/restaurants'
+const REVIEWS_URL = '/reviews'
 
 export const getRestaurants = () => {
     return get(URL)
+}
+
+export const getOwnerRestaurants = (userId) => {
+    return get(`${URL}?owner_id=${userId}`)
+}
+
+export const getPendingReview = (restaurantId) => {
+    return get(`${REVIEWS_URL}?restaurant_id=${restaurantId}`)
+}
+
+export const updateReviews = (reviewId, payload) => {
+    return put(`${REVIEWS_URL}/${reviewId}`, payload)
 }
