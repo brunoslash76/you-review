ee44d096f76872ad484a3a805729dde85202d164
Bruno Russo <btuno.russo@adventhealth.com>
2021-07-03 13:56:46 -0300

    refactor: move header to its own component

--- src/components/layout/authenticated-layout/index.js
+++ src/components/layout/authenticated-layout/index.js
@@ -1,34 +1,17 @@
-import { LogoutButton } from 'components/atoms/logout-button'
-import { RatingStars } from 'components/molecules'
+import { Header } from 'components/molecules'
 import './styles.css'
 
 export const AuthenticatedLayout = (props) => {
-  const { children, title, restaurant, navigation } = props
+  const { children, title, navigation } = props
 
   return (
     <div className="auth-wrapper">
       <div className="auth-container">
         <section className="auth-content">
-          <header className="auth-header">
-            <div className="auth-header-container">
-              <div>
-                {navigation && navigation()}
-                <h1 className="auth-h1">{title ? title : 'You Review'}</h1>
-              </div>
-              <LogoutButton />
-            </div>
-            <div className={!!restaurant ? "auth-subtitle" : "auth-name-right"}>
-              {
-                restaurant
-                && (
-                  <div className="auth-restaurant-avaliation">
-                    <RatingStars averageRating={restaurant.average_rating} />
-                    <p>of <strong>{restaurant.total_reviews}</strong> avaliations</p>
-                  </div>
-                )
-              }
-            </div>
-          </header>
+          <Header
+            navigation={navigation}
+            title={title}
+          />
           <main>
             {children}
           </main>
